97ddb20379f5d0b5708b4dae49816c1f931e49e0
Joshua Cho <joshuasrcho@gmail.com>
2020-05-01 23:17:08 -0700

    websocket communication using multiple end points

--- QuadcopterWebServer/websocket/chat.py
+++ QuadcopterWebServer/websocket/chat.py
@@ -9,7 +9,7 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
-class SimpleWebSocket(tornado.websocket.WebSocketHandler):
+class WSHandler(tornado.websocket.WebSocketHandler):
     connections = set()
     def check_origin(self, origin):
         return True
@@ -18,22 +18,54 @@ class SimpleWebSocket(tornado.websocket.WebSocketHandler):
         self.connections.add(self)
 
     def on_message(self, message):
-        [client.write_message(message) for client in self.connections]
+        [client.write_message(message) for client in FlightWSHandler.connections]
         print(message)
-        #base64Message = base64.b64encode(message)
-        #[client.write_message(base64Message) for client in self.connections]
+
+    def on_close(self):
+        self.connections.remove(self)
+
+class CameraWSHandler(tornado.websocket.WebSocketHandler):
+    connections = set()
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        self.connections.add(self)
+
+    def on_message(self, message):
+        base64Message = base64.b64encode(message)
+        [client.write_message(base64Message) for client in WSHandler.connections]
+
+    def on_close(self):
+        self.connections.remove(self)
+
+class FlightWSHandler(tornado.websocket.WebSocketHandler):
+    connections = set()
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        self.connections.add(self)
+
+    def on_message(self, message):
+        pass
+
     def on_close(self):
         self.connections.remove(self)
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/websocket", SimpleWebSocket)
+        (r"/websocket", WSHandler),
+        (r"/camera", CameraWSHandler),
+        (r"/flight-control", FlightWSHandler),
     ])
 
 if __name__ == "__main__":
+    # If running on a Windows machine, use WindowsSelectorEventLoopPolicy
     if sys.platform == 'win32':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    # Initialize server, listen to port 8888
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
